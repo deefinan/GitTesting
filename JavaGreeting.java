1826d5387a10029fc511bf26f91683eadb301342
Deirdre Finan <deirdre.finan@kmcfe.cdetb.ie>
2023-03-12 18:55:27 +0000

    version two

--- JavaGreeting.java
+++ JavaGreeting.java
@@ -1,5 +1,8 @@
 public class JavaGreeting {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Testing how git works");
+		System.out.println("Testing how git works");
+		
 	}
 }
